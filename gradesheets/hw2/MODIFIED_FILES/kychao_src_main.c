186f3968a12aed2d75ffd440a22031a1b852d914
Gene Stark <gene@starkeffect.com>
2020-04-08 13:37:26 -0400

    Add HW2 gradesheets and artifacts

---
+++ gradesheets/hw2/MODIFIED_FILES/kychao_src_main.c
@@ -0,0 +1,15 @@
+--- hw2/dev_repo/basecode/hw2/src/main.c
+
++++ hw2/repos/kychao/hw2/src/main.c
+
+@@ -1,4 +1,4 @@
+
+-/*
+
++ /*
+
+  * DO NOT MODIFY THE CONTENTS OF THIS FILE.
+
+  * IT WILL BE REPLACED DURING GRADING
+
+  */
