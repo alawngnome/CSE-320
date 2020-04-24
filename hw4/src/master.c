65e83a836d11c5233eb6ab7db47dc391cb1cb5ea
Kevin Chao <kychao@cs.stonybrook.edu>
2020-04-23 20:03:14 -0400

    Well at least I passed the tests

--- hw4/src/master.c
+++ hw4/src/master.c
@@ -6,8 +6,6 @@
 #include "debug.h"
 #include "polya.h"
 
-//volatile sig_atomic_t SIGCHLD_flag = 0; //when SIGCHLD is raised
-
 volatile sig_atomic_t *worker_states; //worker states
 volatile sig_atomic_t *pid_array; //array of worker IDs
 volatile sig_atomic_t *workers_global; //global copy of workers
@@ -18,10 +16,10 @@ void SIGCHLD_handler(int sig) {
     int worker_ID;
     int status;
     //continously handle SIGCHLD signals until no more appear
-    while(1) {
-        if((worker_ID = waitpid(-1, &status, 0|WNOHANG|WUNTRACED|WCONTINUED)) == 0){
-            return; //leave SIGCHLD handler if WNOHANG triggers
-        }
+    while((worker_ID = waitpid(-1, &status, 0|WNOHANG|WUNTRACED|WCONTINUED)) > 0) {
+        // if((worker_ID = waitpid(-1, &status, 0|WNOHANG|WUNTRACED|WCONTINUED)) == 0){
+        //     return; //leave SIGCHLD handler if WNOHANG triggers
+        // }
         //Find the index of the worker
         for(int i = 0; i < *workers_global; i++) {
             if(pid_array[i] == worker_ID){
@@ -41,7 +39,7 @@ void SIGCHLD_handler(int sig) {
                     worker_states[i] = WORKER_STOPPED;
                     STOPPED_flag = 1;
                 }
-                else if(worker_states[i] == WORKER_IDLE && WIFEXITED(status)) {
+                else if(WIFEXITED(status)) {
                     sf_change_state(pid_array[i], worker_states[i], WORKER_EXITED);
                     worker_states[i] = WORKER_EXITED;
                 }
@@ -51,6 +49,9 @@ void SIGCHLD_handler(int sig) {
                 }
             }
         }
+        // if(worker_ID == -1) {
+        //     return;
+        // }
     }
 }
 void SIGPIPE_handler(int sig) {
@@ -63,15 +64,22 @@ void SIGPIPE_handler(int sig) {
  */
 int master(int workers) {
     sf_start(); //called when master has just begun to execute
-    //Initializing
+    //Initializing handlers
     signal(SIGCHLD, SIGCHLD_handler); //SIGCHLD handler
     signal(SIGPIPE, SIGPIPE_handler); //SIGPIPE handler
+    //Intializing pipe fds
     int pid[workers]; //multiple pid for multiple processes
     int problem_fd[workers*2]; //2 fd per process (problem)
     int result_fd[workers*2]; //2 fd per process (worker)
     worker_states = malloc(sizeof(sig_atomic_t) * workers); //create array of states
     pid_array = malloc(sizeof(int)*workers); //create array of worker IDs
     struct problem *problem_array[workers];
+    //set up signal blocking
+    sigset_t SIGCHLD_mask, prev_mask;
+    sigemptyset(&SIGCHLD_mask);
+    sigaddset(&SIGCHLD_mask, SIGCHLD);
+    //sigprocmask(SIG_BLOCK, &SIGCHLD_mask, &prev_mask); //masking SIGCHLD
+    //sigprocmask(SIG_SETMASK, &prev_mask, NULL); //unmasking SIGCHLD
     //make a global copy of workers
     workers_global = malloc(sizeof(workers));
     *workers_global = workers;
@@ -96,7 +104,15 @@ int master(int workers) {
             pid_array[i] = pid[i]; //store child id into pid_array
             //set worker state to STARTED
             sf_change_state(pid_array[i], 0, WORKER_STARTED);
+            sigprocmask(SIG_BLOCK, &SIGCHLD_mask, &prev_mask); //masking SIGCHLD
             worker_states[i] = WORKER_STARTED;
+            sigprocmask(SIG_SETMASK, &prev_mask, NULL); //unmasking SIGCHLD
+        }
+    }
+    //wait for all workers to turned IDLE
+    for(int i = 0; i < workers; i++){
+        if(worker_states[i] == WORKER_STARTED) {
+            pause();
         }
     }
 
@@ -107,21 +123,22 @@ int master(int workers) {
             if(worker_states[i] == WORKER_IDLE){ //if idle worker found
                 //creating the problem
                 struct problem *new_problem = get_problem_variant(workers, i);
-                //store it in problem array
-                problem_array[i] = new_problem;
                 //if no more problem variants can be created
                 if(new_problem == NULL) {
-                    // no_new_problem = 1;
-                    // break;
                     goto no_new_problem;
                 }
+                //store it in problem array
+                problem_array[i] = new_problem;
                 sf_send_problem(pid_array[i], new_problem); //called right before master writes problem
                 //pipe problem into problem pipe
-                fwrite(new_problem, new_problem->size, 1, fdopen(problem_fd[2*i+1], "w"));
-                fflush(fdopen(problem_fd[2*i], "w")); //HOW DOES THIS WORK???
+                FILE *write_FD = fdopen(problem_fd[2*i+1], "w");
+                fwrite(new_problem, new_problem->size, 1, write_FD);
+                fflush(write_FD); //HOW DOES THIS WORK???
                 //set continued worker state
                 sf_change_state(pid_array[i], WORKER_IDLE, WORKER_CONTINUED);
+                sigprocmask(SIG_BLOCK, &SIGCHLD_mask, &prev_mask); //masking SIGCHLD
                 worker_states[i] = WORKER_CONTINUED;
+                sigprocmask(SIG_SETMASK, &prev_mask, NULL); //unmasking SIGCHLD
                 //sending SIGCONT signal
                 kill(pid_array[i], SIGCONT);
             }
@@ -138,51 +155,71 @@ int master(int workers) {
                     FILE *read_FD = fdopen(result_fd[2*i], "r");
                     fread(new_result, sizeof(struct result), 1, read_FD);
                     new_result = realloc(new_result, new_result->size);
-                    //debug("Read the first part of result in...\n");
                     fread(new_result->data, new_result->size - sizeof(struct result), 1, read_FD);
-                    //debug("...Read the second part of result in?\n");
-                    fflush(fdopen(result_fd[2*i], "r"));
+                    //fflush(read_FD);
                     sf_recv_result(pid_array[i], new_result); //called after problem was read
-                    if(post_result(new_result, problem_array[i])){
-                        debug("YAY SOLUTION FOUND\n");
-                    }
+                    post_result(new_result, problem_array[i]);
                     //solution posted, worker goes to IDLE
                     sf_change_state(pid_array[i], WORKER_STOPPED, WORKER_IDLE);
+                    sigprocmask(SIG_BLOCK, &SIGCHLD_mask, &prev_mask); //masking SIGCHLD
                     worker_states[i] = WORKER_IDLE;
+                    sigprocmask(SIG_SETMASK, &prev_mask, NULL); //unmasking SIGCHLD
                 }
-                else if(worker_states[i] == WORKER_RUNNING) { //if other workers are running with a solution already found
+                else if(worker_states[i] == WORKER_RUNNING) {
+                    sf_cancel(pid_array[i]); //current problem solve cancelled
                     kill(pid_array[i], SIGHUP); //send a SIGHUP signal to cancel
-                    // DO WAITPID AND SET
                 }
             }
         }
         STOPPED_flag = 0; //reset STOPPED flag
     }
 
-
     //end workers and master
     no_new_problem:
         //if non-idle worker found
         for(int i = 0; i < workers; i++) {
             if(worker_states[i] != WORKER_IDLE){
+                sf_cancel(pid_array[i]); //current problem solve cancelled
                 kill(pid_array[i], SIGHUP); //end solving if not already IDLE
             }
         }
-        int abnormal_termination = 0; //flag, 1 if abnormal termination
-
+        //wait for all workers to turned IDLE
+        for(int i = 0; i < workers; i++){
+            if(worker_states[i] != WORKER_IDLE) {
+                pause();
+            }
+        }
         //send SIGTERM signal to all workers
         for(int i = 0; i < workers; i++) {
+            //set CONT signal
+            sf_change_state(pid_array[i], WORKER_IDLE, WORKER_CONTINUED);
+            sigprocmask(SIG_BLOCK, &SIGCHLD_mask, &prev_mask); //masking SIGCHLD
+            worker_states[i] = WORKER_CONTINUED; //don't do this so HANDLER sees the state as IDLE to terminate
+            sigprocmask(SIG_SETMASK, &prev_mask, NULL); //unmasking SIGCHLD
             kill(pid_array[i], SIGCONT); //send a continue so worker can process it
-            //send SIGTERM signal & exit
-            if(kill(pid_array[i], SIGTERM) == -1) { //if error
-                abnormal_termination = 1; //abnormal termination found
+            //send SIGTERM signal
+            //sf_change_state(pid_array[i], WORKER_CONTINUED, WORKER_EXITED);
+            kill(pid_array[i], SIGTERM);
+        }
+        //wait for all workers to become exited
+        while(1) {
+            int full_iteration = 1;
+            for(int i = 0; i < workers; i++) {
+                if(worker_states[i] != WORKER_EXITED) {
+                    full_iteration = 0; //if non-exited found reset
+                }
+            }
+            if(full_iteration == 1){
+                break;
             }
         }
         //called as master is about to terminate
         sf_end();
         //if a worker exited with EXIT_FAILURE
-        if(abnormal_termination){
-            exit(EXIT_FAILURE);
+        for(int i = 0; i < workers; i++){
+            if(worker_states[i] == WORKER_ABORTED) {
+                exit(EXIT_FAILURE);
+            }
         }
         //otherwise, return with EXIT_SUCCESS
         exit(EXIT_SUCCESS);
